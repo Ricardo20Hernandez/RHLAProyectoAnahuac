f69c15bfa6c86ee0dd93ef6f8ec13545b541646b
Luis Angel Ramírez Galicia <l.angel.ramirez.g1996@gmail.com>
2023-12-28 15:10:44 -0600

    gett all autor

--- ML/Autor.cs
+++ ML/Autor.cs
@@ -4,6 +4,21 @@
     {
         //prueba
         public int IdAutor { get; set; }
+        public string Nombre { get; set;}
+
+        
+
+        public string ApellidoPaterno { get; set; }
+
+        public string ApellidoMaterno { get; set; }
+
+
+        public string FechaNacimiento { get; set; }
+
+        public byte[] Foto { get; set; }
+
+        public List<object> Autores { get; set; }
+
     }
    
 }
---
+++ PL/Controllers/Autor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PL.Controllers
+{
+    public class Autor : Controller
+    {
+        public IActionResult GetAll()
+        {
+            ML.Result result = BL.Autor.GetAll();
+            if (result.Correct)
+            {
+                ML.Autor autor = new ML.Autor();
+               autor.Autores = result.Objects;
+
+                return View(autor);
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
