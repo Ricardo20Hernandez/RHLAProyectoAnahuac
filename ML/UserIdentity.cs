3237fad4f0e529a1c6710e02e683cb0f6a02b59d
Axell <abrandonaxell@gmail.com>
2023-12-28 18:53:27 -0600

    Rol Identity

---
+++ ML/UserIdentity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    public class UserIdentity
+    {
+        public string IdUsuario { get; set; }
+        public string UserName { get; set; }
+        public ML.Rol Rol { get; set; } //Propiedad de navegación
+        public List<object> IdentityUsers { get; set; }
+    }
+}
