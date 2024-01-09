cbd631d524aa0051e76a02b5ae4eb88288091236
Axell <abrandonaxell@gmail.com>
2024-01-08 18:51:38 -0600

    falta trabajar

---
+++ SL/Controllers/AutorController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutorController : Controller
+    {
+        [EnableCors("API")]
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            ML.Result result = BL.Autor.GetAll();
+            if(result.Correct)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(result);
+            }
+        }
+
+        // POST api/<EmpleadoController>
+        [EnableCors("API")]
+        [HttpPost("add")]
+        public IActionResult Add([FromBody] ML.Autor autor)
+        {
+            ML.Result result = BL.Autor.Add(autor);
+            if (result.Correct)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(result);
+            }
+        }
+
+        [EnableCors("API")]
+        [HttpPut("update")]
+        public IActionResult Update([FromBody] ML.Autor autor)
+        {
+            ML.Result result = BL.Autor.Update(autor);
+            if (result.Correct) { 
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(result);
+            }
+        }
+
+        [EnableCors("API")]
+        [HttpGet("{IdAutor}")]
+        public IActionResult GetById(int IdAutor)
+        {
+            ML.Result result = BL.Autor.GetById(IdAutor);
+
+            if (result.Correct)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return BadRequest(result);
+            }
+        }
+
+
+        [EnableCors("API")]
+        [HttpPost("convert")]
+        public IActionResult Convert(IFormFile fuImagen)
+        {
+            ML.Autor autor = new ML.Autor();
+
+            MemoryStream target = new MemoryStream();
+            fuImagen.OpenReadStream().CopyTo(target);
+            byte[] data = target.ToArray();
+
+            autor.Foto = data;
+
+            if (autor.Foto != null)
+            {
+                return Ok(autor.Foto);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
