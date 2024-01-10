18f77ed8087816e09628fe02fda7d336a4245145
Luis Angel Ramírez Galicia <l.angel.ramirez.g1996@gmail.com>
2024-01-09 18:49:03 -0600

    modulo prestamo pasado a ajax completado

--- PL/Controllers/PrestamoController.cs
+++ PL/Controllers/PrestamoController.cs
@@ -54,6 +54,21 @@ namespace PL.Controllers
             mediosYUsuarios.UserIdentity.IdentityUsers = result1.Objects;
             return View(mediosYUsuarios);
         }
+
+        public IActionResult PrestamoGetAll3()
+
+        {
+            ML.Result result1 = BL.IdentityUser.GetAll();
+            ML.UserIdentity userIdentity1 = new ML.UserIdentity();
+            //userIdentity1.IdentityUsers = result1.Objects;
+            ML.MediosYUsuarios mediosYUsuarios = new ML.MediosYUsuarios();
+            mediosYUsuarios.Medio = new ML.Medio();
+            //mediosYUsuarios.Medio.IdMedio = IdMedio;
+            mediosYUsuarios.UserIdentity = userIdentity1;
+            mediosYUsuarios.UserIdentity.IdentityUsers = result1.Objects;
+            return Ok(mediosYUsuarios);
+        }
+
         [Authorize(Roles = "Administrador")]
         public IActionResult GetAll4(int IdMedio)
 
@@ -68,6 +83,24 @@ namespace PL.Controllers
             mediosYUsuarios.UserIdentity.IdentityUsers = result1.Objects;
             return View(mediosYUsuarios);
         }
+
+        public IActionResult PrestamoGetAll4()
+
+        {
+            ML.Result result1 = BL.IdentityUser.GetAll();
+            ML.UserIdentity userIdentity1 = new ML.UserIdentity();
+            //userIdentity1.IdentityUsers = result1.Objects;
+            ML.MediosYUsuarios mediosYUsuarios = new ML.MediosYUsuarios();
+            mediosYUsuarios.Medio = new ML.Medio();
+            //mediosYUsuarios.Medio.IdMedio = IdMedio;
+            mediosYUsuarios.UserIdentity = userIdentity1;
+            mediosYUsuarios.UserIdentity.IdentityUsers = result1.Objects;
+            return Ok(mediosYUsuarios);
+        }
+
+
+
+
         [Authorize(Roles = "Administrador")]
         public IActionResult MediosPrestados(ML.MediosYUsuarios mediosYUsuarios)
         {
@@ -96,7 +129,63 @@ namespace PL.Controllers
 
         }
 
-       [Authorize(Roles = "Usuario")]
+        public IActionResult MediosPrestadosAjax([FromBody] ML.PrestamoEntrada prestamoEntrada)
+        {
+
+            ML.DetallePrestamo medios = new ML.DetallePrestamo();
+            //ClaimsPrincipal principal = new ClaimsPrincipal();
+            //  string id = principal.FindFirstValue.ToString();
+            //  prestamo.Id = User.getUserId();
+
+            // ClaimsPrincipal principal2 = new ClaimsPrincipal();
+            // ;
+            //va a ser con el id que reciba 
+
+            // ML.Result result = BL.Prestamo.LibrosPrestados(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            ML.Result result = BL.Prestamo.LibrosPrestados(prestamoEntrada.idUsuario);
+            if (result.Correct)
+            {
+                medios.DetallePrestamos = result.Objects;
+            }
+            else
+            {
+                ViewBag.Mensaje = "Sin medios registrados aún";
+            }
+            // aqui tiene que ser ok 
+            return Ok(medios);
+
+        }
+
+        public IActionResult MediosPrestadosDevolverAjax([FromBody] ML.PrestamoEntrada prestamoEntrada)
+        {
+
+            ML.DetallePrestamo medios = new ML.DetallePrestamo();
+            //ClaimsPrincipal principal = new ClaimsPrincipal();
+            //  string id = principal.FindFirstValue.ToString();
+            //  prestamo.Id = User.getUserId();
+
+            // ClaimsPrincipal principal2 = new ClaimsPrincipal();
+            // ;
+            //va a ser con el id que reciba 
+
+            // ML.Result result = BL.Prestamo.LibrosPrestados(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            ML.Result result = BL.Prestamo.LibrosPrestados(prestamoEntrada.idUsuario);
+            if (result.Correct)
+            {
+                medios.DetallePrestamos = result.Objects;
+            }
+            else
+            {
+                ViewBag.Mensaje = "Sin medios registrados aún";
+            }
+            // aqui tiene que ser ok 
+            return Ok(medios);
+
+        }
+
+
+
+        [Authorize(Roles = "Usuario")]
         public IActionResult MisMediosPrestados()
         {
 
@@ -252,6 +341,72 @@ namespace PL.Controllers
            
 
         }
+
+        //para pasar un objeto atravez de una peticion ajax se tiene que
+        //crear el modelo exacto para la peticion 
+        //ademas de untilizar la etiqueta [FromBody]
+        // ML.PrestamoEntrada prestamoEntrada
+        public IActionResult SacarEnPrestamoAjax([FromBody] ML.PrestamoEntrada prestamoEntrada)
+        {
+
+            ML.DetallePrestamo detallePrestamo = new ML.DetallePrestamo();
+            ML.Result result = BL.Medio.GetById(prestamoEntrada.idMedio);
+            ML.Medio medio = new ML.Medio();
+            if (result.Correct)
+            {
+                medio = (ML.Medio)result.Object;
+            }
+            ML.Prestamo prestamo = new ML.Prestamo();
+            // se obtiene el id del usuario4
+            // ClaimsPrincipal principal = new ClaimsPrincipal();
+            //  string id = principal.FindFirstValue.ToString();
+            //  prestamo.Id = User.getUserId();
+
+            // ClaimsPrincipal principal2 = new ClaimsPrincipal();
+            if (medio.CantidadDisponible > 0)
+            {
+                prestamo.Id = prestamoEntrada.idUsuario;
+                // prestamo.Id = HttpContext.Session.GetString("ID");
+                // prestamo.Id = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                prestamo.Medio = new ML.Medio();
+                prestamo.Medio = medio;
+                prestamo.FechaSalida = DateTime.Today.ToString();
+                prestamo.FechaEntregaEsperada = DateTime.Today.AddDays(7).ToString();
+
+                ML.Result resultPrestamo = new ML.Result();
+                resultPrestamo = BL.Prestamo.NuevoPrestamo(prestamo);
+                //   ML.Result resultRestarInventario = new ML.Result();
+                // resultRestarInventario = BL.Prestamo.RestarInventario(IdMedio,prestamo.Id);
+
+                detallePrestamo.Prestamo = new ML.Prestamo();
+                detallePrestamo.Prestamo = prestamo;
+                ML.Result librosPrestados = new ML.Result();
+                librosPrestados = BL.Prestamo.LibrosPrestados(prestamo.Id);
+                detallePrestamo.DetallePrestamos = librosPrestados.Objects;
+                //medio.TipoMedio.TipoMedios = resultTipoMedios.Objects;
+                //medio.Editorial.Editoriales = resultEditoriales.Objects;
+                //medio.Idioma.Idiomas = resultIdiomas.Objects;
+                //medio.Autor.Autores = resultAutores.Objects;
+                if (!resultPrestamo.Correct)
+                {
+                    ViewBag.Accion = resultPrestamo.ErrorMessage;
+                }
+                ML.Result tipoMedio = new ML.Result();
+            }
+
+            // tipoMedio = BL.TipoMedio.GetAll();
+            // detallePrestamo.Prestamo.Medio.TipoMedio.TipoMedios = tipoMedio.Objects;
+            //detallePrestamo.Prestamo.Medio.TipoMedio
+            // mediosYUsuarios.
+            //aqui se tiene que reecostruir la vista del detalle prestamo con ajax
+            return Ok(detallePrestamo);
+
+
+
+        }
+
+
+
         [Authorize(Roles = "Administrador")]
         public IActionResult Devolver(int IdMedio,string Id)
         {// ClaimsPrincipal principal = new ClaimsPrincipal();
@@ -282,6 +437,40 @@ namespace PL.Controllers
 
 
         }
+
+        public IActionResult DevolverElPrestamoAjax([FromBody] ML.PrestamoEntrada prestamoEntrada)
+        {// ClaimsPrincipal principal = new ClaimsPrincipal();
+
+            //  string id = principal.FindFirstValue.ToString();
+            //  prestamo.Id = User.getUserId();
+
+            // ClaimsPrincipal principal2 = new ClaimsPrincipal();
+            //prestamo.Id = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            ML.DetallePrestamo detallePrestamo = new ML.DetallePrestamo();
+            detallePrestamo.Prestamo = new ML.Prestamo();
+            detallePrestamo.Prestamo.Medio = new ML.Medio();
+
+            detallePrestamo.FechaEntregaReal = DateTime.Today.ToString();
+            detallePrestamo.Entregado = true;
+            detallePrestamo.Prestamo.Medio.IdMedio = prestamoEntrada.idMedio;
+            detallePrestamo.Prestamo.Id = prestamoEntrada.idUsuario;
+            ML.Result result = new ML.Result();
+
+            result = BL.DetallePrestamo.DevolverMedio(detallePrestamo);
+            if (result.Correct)
+            {
+                ViewBag.Mensaje = "Medio devuelto Correctamente el dia de" + detallePrestamo.FechaEntregaReal;
+            }
+
+
+            return View("Modal");
+
+
+        }
+
+
+
+
 
         //simulacion de servicio desde el controlador 
 
